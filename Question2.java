bb3df548ff344474f6356676d3f3e191aaff02f2
Chow Keng <>
2021-04-23 15:00:14 +0000

    pls green tick

--- Question2.java
+++ Question2.java
@@ -13,7 +13,7 @@ public class Question2
     Scanner in = new Scanner(System.in);
     System.out.print("Enter height: ");
     double height = in.nextDouble();
-    height = height/100;
+    // height = height/100;
     System.out.print("Enter weight: ");
     double weight = in.nextDouble();
 
