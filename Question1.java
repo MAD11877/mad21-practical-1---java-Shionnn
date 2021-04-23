b3faf873f411f588054ffbf5f08cfea96a281fd9
Chow Keng <>
2021-04-23 14:53:43 +0000

    Trying to fix so that github will give me green tick

--- Question1.java
+++ Question1.java
@@ -1,5 +1,10 @@
 public class Question1
 {
+  /**
+     * Print "Hello World!"
+     * 
+     * Hint: System.out.println(...)
+     */
   public static void main(String[] args)
   {
     System.out.println("Hello World!");
--- Question3.java
+++ Question3.java
@@ -11,6 +11,7 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
+    
     System.out.print("Enter the integer: ");
     int num = in.nextInt();
     System.out.println("Result: " + num*num);
